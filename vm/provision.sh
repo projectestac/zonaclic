cdf1c88de20867a737f7ac75de4844d8610ec23b
Francesc Busquets <francesc@gmail.com>
2018-03-19 16:24:15 +0100

    Remove 'squid' package from Vagrant (not used)

--- vm/provision.sh
+++ vm/provision.sh
@@ -6,11 +6,10 @@ source "/vm/provision/functions.sh"
 
 /vm/provision/php7.sh
 /vm/provision/mysql.sh $pass
-/vm/provision/squid.sh
 
 /vm/clic/provision.sh
 
 sudo service apache2 restart
 
 # Here we must write the latest version number
-save_version 2018030600
+save_version 2018031900
