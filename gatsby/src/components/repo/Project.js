f3efded4eeaa1c912ddaca72f226f6fe058e1698
Francesc Busquets <francesc@gmail.com>
2020-02-25 16:32:50 +0100

    :zap: Work on project pages

--- gatsby/src/components/repo/Project.js
+++ gatsby/src/components/repo/Project.js
@@ -3,48 +3,45 @@ import { withPrefix } from 'gatsby';
 import { Link } from 'gatsby-plugin-intl';
 import { makeStyles } from "@material-ui/core/styles";
 import { mergeClasses, htmlContent } from '../../utils/misc';
+import { FontAwIcon } from '../../utils/FontAwIcon';
 import filesize from 'filesize';
 import SEO from '../SEO';
 import ShareButtons from '../ShareButtons';
+import PlayIcon from '@material-ui/icons/PlayCircleFilled';
+import JavaIcon from '@material-ui/icons/LocalCafe';
+import DownloadIcon from '@material-ui/icons/CloudDownload';
+import IconButton from '@material-ui/core/IconButton';
+import Button from '@material-ui/core/Button';
 
-const useStyles = makeStyles(_theme => ({
+const useStyles = makeStyles(theme => ({
   root: {
   },
   cover: {
-    position: 'relative',
     maxHeight: '10rem',
+  },
+  btnContainer: {
+    display: 'inline-flex',
+    alignItems: 'center',
+    justifyContent: 'center',
     marginBottom: '1rem',
   },
   overlayBtn: {
     position: 'absolute',
-    padding: '0.5rem',
-    opacity: '10%',
-    height: '50%',
-    width: '50%',
-    left: '50%',
-    top: '50%',
-    transform: 'translate(-50%,-50%)',
-    cursor: 'pointer',
-    background: 'transparent',
-    border: 'none',
-    "& img": {
-      maxWidth: '100%',
-      maxHeight: '100%',
+    opacity: '20%',
+    height: '10rem',
+    width: '10rem',
+    "& svg": {
+      fontSize: 96,
     },
     "&:hover": {
-      opacity: '75%',
+      opacity: '90%',
     },
-  },
-  btnContainer: {
-    display: 'inline-block',
-    position: 'relative',
   },
   mainBlock: {
     marginTop: '1rem',
     marginBottom: '1rem',
   },
   description: {
-    minWidth: '30%',
     "& li": {
       marginBottom: '1rem',
     }
@@ -52,6 +49,8 @@ const useStyles = makeStyles(_theme => ({
   dataCard: {
     borderCollapse: 'collapse',
     minWidth: '80%',
+    marginTop: '1.5rem',
+    marginBottom: '1rem',
     "& td": {
       border: 'none',
       borderBottom: '1px solid lightgray',
@@ -77,7 +76,14 @@ const useStyles = makeStyles(_theme => ({
     margin: 0,
     paddingLeft: 0,
     listStyleType: 'none',
-  }
+  },
+  buttons: {
+    display: 'flex',
+    flexWrap: 'wrap',
+    "& a,button": {
+      marginRight: '1rem',
+    }
+  },
 }));
 
 const shareSites = { moodle: true, classroom: true, embed: true };
@@ -92,6 +98,7 @@ function Project({ intl, project, SLUG, REPO_BASE, location, fullProjectList, ..
     cover, thumbnail,
     activities, mediaFiles, totalSize,
   } = project;
+
   const { locale, defaultLocale, messages } = intl;
   const classes = mergeClasses(props, useStyles());
   const k = meta_langs.includes(locale) ? locale : defaultLocale;
@@ -100,6 +107,8 @@ function Project({ intl, project, SLUG, REPO_BASE, location, fullProjectList, ..
   const pageDesc = description[k];
   const imgPath = cover && `${fullPath}/${cover}`;
   const moodleLink = `${fullPath}/${mainFile}`;
+  const projectLink = moodleLink.replace(/\/[^/]*$/, '/index.html');
+  const instJavaLink = `https://clic.xtec.cat/jnlp/jclic/install.jnlp?argument=${fullPath}/${instFile}`;
   const embedOptions = {
     width: '800',
     height: '600',
@@ -108,7 +117,6 @@ function Project({ intl, project, SLUG, REPO_BASE, location, fullProjectList, ..
     src: `${fullPath}/jclic.js/index.html`,
   }
   const getProjectTitle = path => (fullProjectList && fullProjectList?.find(prj => prj.path === path)?.title) || path;
-
 
   return (
     <div {...props} className={classes.root}>
@@ -119,10 +127,16 @@ function Project({ intl, project, SLUG, REPO_BASE, location, fullProjectList, ..
       <div className={classes['mainBlock']}>
         {imgPath &&
           <div className={classes['btnContainer']}>
-            <img src={imgPath} alt={messages['cover-alt']} className={classes['cover']} />
-            <button className={classes['overlayBtn']} onClick={() => alert('hola!')}>
-              <img src="/img/play-button.png" alt="play" />
-            </button>
+            <img src={imgPath} className={classes['cover']} alt={messages['cover-alt']} />
+            <IconButton
+              className={classes['overlayBtn']}
+              color="secondary"
+              href={projectLink} target="_BLANK"
+              aria-label={messages['prj-launch']}
+              title={messages['prj-launch']}
+            >
+              <PlayIcon />
+            </IconButton>
           </div>
         }
         <div className={classes['description']} dangerouslySetInnerHTML={{ __html: htmlContent(description[k]) }}></div>
@@ -204,6 +218,34 @@ function Project({ intl, project, SLUG, REPO_BASE, location, fullProjectList, ..
           }
         </tbody>
       </table>
+      <div className={classes['buttons']}>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<PlayIcon />}
+          href={projectLink} target="_BLANK"
+          aria-label={messages['prj-launch']} title={messages['prj-launch']}
+        >
+          Launch
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<DownloadIcon />}
+          aria-label={messages['prj-download']} title={messages['prj-download']}
+        >
+          Download
+        </Button>
+        <Button
+          variant="contained"
+          color="primary"
+          startIcon={<JavaIcon />}
+          href={instJavaLink} target="_BLANK"
+          aria-label={messages['prj-java-inst']} title={messages['prj-java-inst']}
+        >
+          Java Install
+        </Button>
+      </div>
     </div>
   );
 }
